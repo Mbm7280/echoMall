4efbf9499b7db3d2e883efd8b433dc75da8d6d07
Echo <mbm1216@163.com>
2022-10-25 23:55:12 +0800

    init

---
+++ src/main/java/com/echo/mall/modules/pms/controller/PmsBrandController.java
@@ -0,0 +1,43 @@
+package com.echo.mall.modules.pms.controller;
+
+
+import cn.hutool.core.collection.CollectionUtil;
+import com.echo.mall.modules.pms.model.PmsBrand;
+import com.echo.mall.modules.pms.service.PmsBrandService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 品牌表 前端控制器
+ * </p>
+ *
+ * @author echo
+ * @since 2022-10-25
+ */
+@RestController
+@RequestMapping("/pms/pmsBrand")
+public class PmsBrandController {
+
+    @Autowired
+    private PmsBrandService pmsBrandService;
+
+
+    @ApiOperation("查询品牌列表")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public String queryBrandList() {
+        List<PmsBrand> brandList = pmsBrandService.list();
+        if(CollectionUtil.isEmpty(brandList)){
+            return "failed";
+        }else {
+            return "success";
+        }
+    }
+
+
+}
+
